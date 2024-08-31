e6c16ce261599b24441fe7cbeb21190f7881e4cc
Vlad P <vlad@pysarenko.com>
2024-09-01 01:11:12 +0200

    memoised selector

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -3,7 +3,7 @@ import { createSelector } from '@reduxjs/toolkit';
 export const selectFilter = state => state.filter;
 
 export const selectContacts = state => state.contacts.items;
-export const selectContactsExist = state => state.contacts.items.length > 0;
+export const selectContactsExist = createSelector([selectContacts], contacts => contacts.length > 0);
 export const selectFilteredContacts = createSelector([selectContacts, selectFilter], (contacts, filter) =>
   contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase())).sort((a, b) => a.name.localeCompare(b.name))
 );
